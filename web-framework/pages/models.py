90f912d4c2288e3c5f69ce0a8b1bd54efe01ccfd
Young Hyeon <44453931+yhnii@users.noreply.github.com>
2019-02-12 18:04:13 +0900

    searching class
    검색 기능 추가

--- web-framework/pages/models.py
+++ web-framework/pages/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Books(models.Model):
+    bCode = models.IntegerField(primary_key=True)
+    bName = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    publisher = models.CharField(max_length=255)
+    publication_year = models.IntegerField()
+    classno = models.IntegerField()
+    loanCnt = models.IntegerField()
+    bImage = models.CharField(max_length=255)
+
--- web-framework/pages/urls.py
+++ web-framework/pages/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomePageView.as_view(), name='home'),
+    path('', views.HomePageView.as_view(), name='home'),   #http://127.0.0.1:8000/
+    path('books/', views.BookListView.as_view(), name='books'),   #http://127.0.0.1:8000/books/
 ]
