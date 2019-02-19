b2de2a057a5f8c4cc1be37fce3058cc54955860d
Young Hyeon <44453931+yhnii@users.noreply.github.com>
2019-02-19 18:09:59 +0900

    메인 페이지 도서 리스트 뽑기
    메인페이지에 임시로 올려져있던 이미지 파일을 db와 연동해서 랜덤으로 4개가 보여지게 끔 함

--- web-framework/pages/views.py
+++ web-framework/pages/views.py
@@ -9,6 +9,20 @@ from pages.models import Books
 class HomePageView(TemplateView):
     print('---HomePageView---')
     template_name = 'home.html'
+    
+    def get_context_data(self, **kwargs):
+        print('--get_context_data--')
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        #context['object_list'] = Book.objects.order_by('-loanCnt')[:5]
+        my_ids = Book.objects.values_list('bName', flat=True)
+        my_ids = list(my_ids)
+        n = 4   
+        rand_ids = random.sample(my_ids, n)
+        print('--random id :', rand_ids)
+        random_records = Book.objects.filter(bName__in=rand_ids)
+        print('---random :',random_records)       
+        context['object_list'] = random_records
+        return context
 
 
 class SearchPageView(TemplateView):
